f3bd6c59a78a36e41a1785e6594902dc3a220aec
Uneezeh69 <62845997+Uneezeh69@users.noreply.github.com>
2020-08-01 03:52:06 +0500

    Scroll loading error removed.

--- Maincode.js
+++ Maincode.js
@@ -17,7 +17,7 @@
         }
 
         // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+var countDownDate = new Date("Sept 5, 2020 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
